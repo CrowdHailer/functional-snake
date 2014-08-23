fe56352326649b0858fdba65311ef5e9256fa010
Peter <peterhsaxton@gmail.com>
2014-08-23 23:28:28 +0100

    times utility

--- test/spec/utilities_spec.js
+++ test/spec/utilities_spec.js
@@ -12,5 +12,28 @@ describe('Utilities', function () {
             expect(constant()).toEqual(3);
         });
     });
+
+    describe('Identity', function () {
+        it('should return given value', function () {
+            var obj = {}
+            expect(_.i(obj)).toBe(obj);
+        });
+    });
+
+    describe('Times', function () {
+        // Use optional arguments in call
+        it('should call a function multiple times', function () {
+            var dummy = jasmine.createSpy();
+            _.times(3, dummy);
+            expect(dummy.calls.count()).toEqual(3);
+        });
+
+        it('should pass index', function () {
+            var dummy = jasmine.createSpy();
+            _.times(3, dummy);
+            expect(dummy).toHaveBeenCalledWith(0);
+            expect(dummy).toHaveBeenCalledWith(2);
+        });
+    });
 });
 
